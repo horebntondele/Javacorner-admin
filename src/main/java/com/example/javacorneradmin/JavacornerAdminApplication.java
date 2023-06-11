c035402fc7b64119ed0858d41ecc07d07ea11147
horebntondele <horebntondele@gmail.com>
2023-06-11 16:25:37 +0100

    Java Corner project

---
+++ src/main/java/com/example/javacorneradmin/JavacornerAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacorneradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacornerAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacornerAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javacorneradmin/JavacornerAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacorneradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacornerAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
